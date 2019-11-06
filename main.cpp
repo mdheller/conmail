527449b9422ff107af2b2e5a5ae2a61c9b284d6a
Him <mhimanshu712@gmail.com>
2019-11-06 14:23:02 +0530

    Display function added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void display(char * p);
+
 int main(){
-	printf("yoyo");
+	display("./logo.map");
 }
+
+void display(char * p){
+	FILE * fptr;
+	fptr = fopen(p,"r");
+	char buff[100];
+
+	while(!feof(fptr)){
+		fgets(buff,100,fptr);
+		printf("%s",buff);
+	}
+
+	printf("\n");;
+}
+
